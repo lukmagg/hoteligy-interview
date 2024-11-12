e4d714f388b8c7b42ebc5970687b8f6b2067068a
Lucas Antonio Maggio <marsinformatica@gmail.com>
2024-11-12 19:28:31 +0100

    init project, install bootstrap

---
+++ src/views/MainView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div>
+      <NavBar></NavBar>
+    </div>
+    <div class="content">
+      <div>
+        <RouterView></RouterView>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import NavBar from '../components/NavBar.vue'
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+}
+</style>
